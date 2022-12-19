767eb562e654b4aa38d7dc86c9e20eeb35b8912d
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-12-20 01:55:07 +0600

    designed and setup left and right side nav

--- src/Shared/LeftSideNav/LeftSideNav.js
+++ src/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5111/news-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+        
+    },[])
+
     return (
         <div>
-            <h2>Left side Nav</h2>
+            <h2>All category {categories.length} </h2>
+            <div>
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link to={`/category/:id`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
--- src/Shared/RightSideNav/RightSideNav.js
+++ src/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { ButtonGroup , ListGroup } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { FaGithub , FaGoogle, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch} from 'react-icons/fa';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right side Nav</h2>
+            <ButtonGroup vertical>
+            <Button variant="outline-primary"><FaGoogle></FaGoogle> Sign In with Google</Button>
+            <br />
+            <Button variant="outline-dark"><FaGithub></FaGithub> Sign In with GitHub</Button>
+            </ButtonGroup>
+
+            <div>
+                <h5 className='mt-4'>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className="mb-2"><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaWhatsapp></FaWhatsapp> WhatsApp</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaTwitch></FaTwitch> Twitch</ListGroup.Item>
+                    <ListGroup.Item className="mb-2">Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </div>
+        
         </div>
     );
 };
