217b16109a1be5b953705a9a1dc5367b6b6cd6cd
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-12-20 02:21:08 +0600

    Responsive NavBar and Carousel for adertisement

---
+++ src/Shared/BrandCarousel/BrandCarousel.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Brand1 from '../../assets/brands/brand1.png';
+import Brand2 from '../../assets/brands/brand2.png';
+
+const BrandCarousel = () => {
+    return (
+        <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Brand1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3></h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Brand2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3></h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    );
+};
+
+export default BrandCarousel;
--- src/Shared/RightSideNav/RightSideNav.js
+++ src/Shared/RightSideNav/RightSideNav.js
@@ -1,30 +1,53 @@
-import React from 'react';
-import { ButtonGroup , ListGroup } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import { FaGithub , FaGoogle, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch} from 'react-icons/fa';
+import React from "react";
+import { ButtonGroup, ListGroup } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import {
+  FaGithub,
+  FaGoogle,
+  FaFacebook,
+  FaTwitter,
+  FaWhatsapp,
+  FaTwitch,
+} from "react-icons/fa";
+import BrandCarousel from "../BrandCarousel/BrandCarousel";
 
 const RightSideNav = () => {
-    return (
-        <div>
-            <ButtonGroup vertical>
-            <Button variant="outline-primary"><FaGoogle></FaGoogle> Sign In with Google</Button>
-            <br />
-            <Button variant="outline-dark"><FaGithub></FaGithub> Sign In with GitHub</Button>
-            </ButtonGroup>
+  return (
+    <div>
+      <ButtonGroup vertical>
+        <Button variant="outline-primary">
+          <FaGoogle></FaGoogle> Sign In with Google
+        </Button>
+        <br />
+        <Button variant="outline-dark">
+          <FaGithub></FaGithub> Sign In with GitHub
+        </Button>
+      </ButtonGroup>
 
-            <div>
-                <h5 className='mt-4'>Find us on</h5>
-                <ListGroup>
-                    <ListGroup.Item className="mb-2"><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
-                    <ListGroup.Item className="mb-2"><FaWhatsapp></FaWhatsapp> WhatsApp</ListGroup.Item>
-                    <ListGroup.Item className="mb-2"><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
-                    <ListGroup.Item className="mb-2"><FaTwitch></FaTwitch> Twitch</ListGroup.Item>
-                    <ListGroup.Item className="mb-2">Vestibulum at eros</ListGroup.Item>
-                </ListGroup>
-            </div>
-        
-        </div>
-    );
+      <div>
+        <h5 className="mt-4">Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook></FaFacebook> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp></FaWhatsapp> WhatsApp
+          </ListGroup.Item>
+
+          <ListGroup.Item className="mb-2">
+            <FaTwitter></FaTwitter> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitch></FaTwitch> Twitch
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <BrandCarousel></BrandCarousel>
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;
