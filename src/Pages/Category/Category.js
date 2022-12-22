dec4982fdd9e3b8a4a8302e4ec8ca7280102700f
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-12-22 11:44:53 +0600

    designed news section

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCart/NewsSummaryCart';
 
 const Category = () => {
-    const news = useLoaderData();
+    const categoryNews = useLoaderData();
     return (
         <div>
-            <h2>This is the category has news: {news.length} news</h2>
+            <h2>This is the category has news: {categoryNews.length} news</h2>
+            {
+                categoryNews.map(news => <NewsSummaryCard key={news._id} news={news}></NewsSummaryCard> )
+            }
         </div>
     );
 };
--- src/Shared/NewsSummaryCart/NewsSummaryCart.js
+++ src/Shared/NewsSummaryCart/NewsSummaryCart.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import { FaEye, FaRegBookmark, FaShareAlt, FaStar } from "react-icons/fa";
 
 const NewsSummaryCard = ({ news }) => {
-    const { _id, title, author, details, image_url, total_view } = news;
+    const { _id, title, author, details, image_url, rating, total_view } = news;
     return (
-        <Card className="text-center">
-      <Card.Header>Featured</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
-    </Card>
+        <Card className="mt-2">
+            <Card.Header className="d-flex justify-content-between align-items-center">
+                <div className="d-flex">
+                    <Image
+                        className="me-2"
+                        roundedCircle
+                        src={author?.img}
+                        style={{ height: "60px" }}
+                    />
+                    <div>
+                        <p className="mb-0">{author?.name}</p>
+                        <p>{author?.published_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <FaRegBookmark className="me-2"></FaRegBookmark>
+                    <FaShareAlt></FaShareAlt>
+                </div>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Text>
+                    {details.length > 150 ? (
+                        <p>
+                            {details.slice(0, 200) + "..."}{" "}
+                            <Link to={`/news/${_id}`}>read more</Link>{" "}
+                        </p>
+                    ) : (
+                        <p>{details}</p>
+                    )}
+                </Card.Text>
+                
+            </Card.Body>
+            <Card.Footer className="d-flex justify-content-between">
+                <div>
+                    <FaStar className="text-warning me-2"></FaStar>
+                    <span>{rating?.number}</span>
+                </div>
+                <div>
+                    <FaEye className="me-2"></FaEye>
+                    <span>{total_view }</span>
+                </div>
+            </Card.Footer>
+        </Card>
     );
 };
 
